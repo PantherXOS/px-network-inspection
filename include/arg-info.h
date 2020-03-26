90575a5e85b82d2ec767311fe90346f4cc5dc70c
sinainnet <sinainnet@gmail.com>
2020-03-26 12:13:54 +0430

    change version

--- include/arg-info.h
+++ include/arg-info.h
@@ -6,7 +6,7 @@
 #include <string.h>
 
 ///	@brief	version of the px-network-inspection
-const char *argp_program_version = "0.0.7";
+const char *argp_program_version = "0.0.8";
 
 ///	@brief	the bug report email address.
 const char *argp_program_bug_address = "<s.mahmood@pantherx.org>";
